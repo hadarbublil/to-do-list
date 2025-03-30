7e53f28a2f0923f1bfb10b5499e9b94e71f4dd3e
hadar <hadarbublil2@gmail.com>
2025-03-30 13:28:04 +0300

    center text in td

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -7,23 +7,23 @@ const Task = ({ task, onDelete, onEdit }) => {
   const priorityLabels = ['Low', 'Medium', 'High', 'Urgent'];
   const statusLabels = ['Draft', 'In Progress', 'On Hold', 'Completed', 'Deleted'];
   return (
-    <tr className="text-left">
-      <td title={task.title} className='text-ellipsis'>{task.title}</td>
-      <td title={task.description} className='text-ellipsis'>{task.description}</td>
-      <td>{new Date(task.due_date).toLocaleDateString()}</td>
-      <td>{new Date(task.create_date).toLocaleDateString()}</td>
-      <td>{new Date(task.update_date).toLocaleDateString()}</td>
-      <td>
+    <tr className="text-center">
+      <td title={task.title} className='text-ellipsis text-center-td'>{task.title}</td>
+      <td title={task.description} className='text-ellipsis text-center-td'>{task.description}</td>
+      <td className='text-center-td'>{new Date(task.due_date).toLocaleDateString()}</td>
+      <td className='text-center-td'>{new Date(task.create_date).toLocaleDateString()}</td>
+      <td className='text-center-td'>{new Date(task.update_date).toLocaleDateString()}</td>
+      <td className='text-center-td'>
         <span className={`tag priority-${task.priority_id}`}>
           {priorityLabels[task.priority_id - 1]}
         </span>
       </td>
-      <td>
+      <td className='text-center-td'>
         <span className={`tag status-${task.status_id}`}>
           {statusLabels[task.status_id - 1]}
         </span>
       </td>
-      <td>{task.assigned_user_id}</td>
+      <td className='text-center' style={{verticalAlign: "middle"}}>{task.assigned_user_id}</td>
       <td>
         <div style={{ display: "flex", gap: "5px", flexWrap: "wrap", justifyContent: "center" }}>
           <button className="btn btn-outline-success custom-border task-btn" data-bs-toggle="button" onClick={() => onEdit(task)}>Edit</button>
